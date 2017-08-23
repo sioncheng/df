50ef90a344523d464720908ba0f0fded94aef002
程勇桥 <cyq@dst64953.cn1.global.ctrip.com>
2017-08-23 19:33:10 +0800

    twitter zookeeper client is not good

--- fs/src/main/scala/com/github/sioncheng/fs/StartApp.scala
+++ fs/src/main/scala/com/github/sioncheng/fs/StartApp.scala
@@ -1,34 +1,31 @@
 package com.github.sioncheng.fs
 
-import org.apache.zookeeper.Watcher.Event.KeeperState
+import java.net.{InetAddress, InetSocketAddress}
+import java.nio.charset.Charset
+
+import com.loopfor.zookeeper.{ACL, AsynchronousZookeeper, Configuration, Ephemeral, SynchronousZookeeper, Zookeeper}
 import org.apache.zookeeper.ZooDefs.Ids
 
 import scala.collection.JavaConverters
+
 
 object StartApp extends App {
 
     println("fs app")
 
-    import com.twitter.util.JavaTimer
-    import com.twitter.conversions.time._
-    import com.twitter.zk._
+    val servers = List(new InetSocketAddress("127.0.0.1", 2181))
+    val conf = Configuration(servers)
 
-    implicit val timer = new JavaTimer(true)
-    val zk =  ZkClient("127.0.0.1:2181", Some(5.seconds), 30.seconds)
+    val zk = Zookeeper(conf)
 
+    val client = zk.sync
 
-    val pf : PartialFunction[StateEvent, Unit] = {
-        case x => println(x)
-            println(x.state == KeeperState.SyncConnected)
+    if(!client.exists("/root").isEmpty) {
+        println(client.create("/root/a",
+            "".getBytes(),
+            ACL.AnyoneAll,
+            Ephemeral))
     }
-    zk.onSessionEvent(pf)
-
-    println(zk.name)
-
-    val zNode = zk.apply("/root")
-    val acl =  JavaConverters.asScalaBuffer(Ids.OPEN_ACL_UNSAFE)
-    zNode.exists.apply().onSuccess(z => println(s"exists ? ${z}"))
-    zNode.create(acls = acl).onSuccess(z => println(s"success ${z.name}")).onFailure(z => println(z.getMessage))
 
     io.StdIn.readLine()
 }
